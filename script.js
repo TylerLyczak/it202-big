87b4820ecb5c6daa91753e6fc2626aeef53728ab
TylerLyczak <tyler.m.lyczak@gmail.com>
2019-04-16 02:29:50 -0500

    Added a dictioney api so users can search for words

--- script.js
+++ script.js
@@ -39,9 +39,16 @@ $("#requestDog").on("click", function(){
   requestForDogPicture();
 });
 
+// Button listener that submits the user inputed word to the api function
+$("#requestDictionary").on("click", function(){
+  var word = $("#dictionaryWord").val();
+  if (word !== "")  {
+    requestForWordDictionary(word);
+  }
+});
+
 // Makes a call to the api to get a dog picture/video
 function requestForDogPicture ()  {
-
   $.get("https://random.dog/woof.json",
 
     function(response) {
@@ -59,6 +66,23 @@ function requestForDogPicture ()  {
         $("#dogImage").attr("src",url);
         $("#dogVideo").hide();
         $("#dogImage").show();
+      }
+    });
+}
+
+// Makes a call to Merriam-Webster api with an user inputed word.
+function requestForWordDictionary(word) {
+  var url = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + word + "?key=18e62b19-340b-4024-978f-67826d0b6401";
+  $.get(url,
+
+    function(response) {
+      console.log(response);
+      var defs = response[0].shortdef;
+      $("#dictionaryResponse").empty();
+      for (var i=0; i<defs.length; i++) {
+        var int = i+1;
+        var paraVar = "<p id=" + i + ">" + int + ". " + defs[i] + "</p>";
+        $("#dictionaryResponse").append(paraVar);
       }
     });
 }
